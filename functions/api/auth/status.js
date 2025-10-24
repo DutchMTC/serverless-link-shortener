c317735663b477e61dbb061cac53f421632f8d7d
Miyaki <90682254+DutchMTC@users.noreply.github.com>
2025-10-24 22:21:53 +0200

    pw fix

--- functions/api/auth/status.js
+++ functions/api/auth/status.js
@@ -3,10 +3,11 @@
 export async function onRequestGet(context) {
   const { env } = context;
 
-  // ADMIN_PASSWORD will be an environment variable set in the Cloudflare dashboard.
+  // Check for both the admin password and the public link creation password.
   const isPasswordSet = env.ADMIN_PASSWORD && env.ADMIN_PASSWORD.length > 0;
+  const passwordProtected = env.LINK_SHORTENER_PASSWORD && env.LINK_SHORTENER_PASSWORD.length > 0;
 
-  return new Response(JSON.stringify({ isPasswordSet }), {
+  return new Response(JSON.stringify({ isPasswordSet, passwordProtected }), {
     headers: {
       'Content-Type': 'application/json',
       'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',

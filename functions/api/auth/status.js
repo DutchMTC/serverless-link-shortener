de26090b3a7e9913a79cf1c80a29b968a153e908
Miyaki <90682254+DutchMTC@users.noreply.github.com>
2025-10-24 22:11:06 +0200

    fix

--- functions/api/auth/status.js
+++ functions/api/auth/status.js
@@ -3,10 +3,10 @@
 export async function onRequestGet(context) {
   const { env } = context;
 
-  // PASSWORD will be an environment variable set in the Cloudflare dashboard.
-  const passwordProtected = env.PASSWORD && env.PASSWORD.length > 0;
+  // ADMIN_PASSWORD will be an environment variable set in the Cloudflare dashboard.
+  const isPasswordSet = env.ADMIN_PASSWORD && env.ADMIN_PASSWORD.length > 0;
 
-  return new Response(JSON.stringify({ passwordProtected }), {
+  return new Response(JSON.stringify({ isPasswordSet }), {
     headers: {
       'Content-Type': 'application/json',
       'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
